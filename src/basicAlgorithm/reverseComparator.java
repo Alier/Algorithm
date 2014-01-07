20ea91c4cca5328a04b1024afe79082bbfa647da
AlierZ <ttt@hotmail.com>
2014-01-06 23:30:43 -0800

    initial commit

---
+++ src/basicAlgorithm/reverseComparator.java
@@ -0,0 +1,13 @@
+package basicAlgorithm;
+import java.util.Comparator;
+
+
+public class reverseComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer x , Integer y) {
+		// TODO Auto-generated method stub
+		return(Integer.compare(y, x));
+	}
+
+}
