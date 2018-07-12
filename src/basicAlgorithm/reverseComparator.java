6b6d8f9420b64498ea6fa2bde6b9fb2084b1a640
alierzhang <qitianxiaosheng@yahoo.com>
2018-07-12 14:22:01 -0700

    one change

--- src/basicAlgorithm/reverseComparator.java
+++ src/basicAlgorithm/reverseComparator.java
@@ -7,7 +7,8 @@ public class reverseComparator implements Comparator<Integer> {
 	@Override
 	public int compare(Integer x , Integer y) {
 		// TODO Auto-generated method stub
-		return(Integer.compare(y, x));
+		//return(Integer.compare(y, x)); // temp
+		return 0;
 	}
 
 }
